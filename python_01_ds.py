cd98f509c908c2c0747d6c65ce2b56e2525e4efe
Oleg Yurchik <oleg.yurchik@protonmail.com>
2022-07-01 21:27:45 +0300

    summa was added

--- python_01_ds.py
+++ python_01_ds.py
@@ -3,6 +3,11 @@ print('Hello world!')
 print("Let's create new repo!")
 print('Something else was added')
 
+a = 34
+b = 46
+
+print a + b
+
 print(my_strinasg)
 
 my_string = 'Helaslo! My name is Alex'
